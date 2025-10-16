54d80ea35015460f4fc86341e32b6e33b23bde25
sun-moon66 <eleman.co3266@gmail.com>
2025-10-16 08:21:34 +0330

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-TON_WALLET = os.getenv("TON_WALLET")
-TON_API_KEY = os.getenv("TON_API_KEY")
-CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "30"))
